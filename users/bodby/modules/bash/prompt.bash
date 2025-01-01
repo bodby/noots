8228d2ca09404131f10e8dfb45b937d77375bb54
Baraa Homsi <baraa.homsi@proton.me>
2025-01-01 16:55:15 +0000

    feat: Added file diffs to Git prompt section

--- users/bodby/modules/bash/prompt.bash
+++ users/bodby/modules/bash/prompt.bash
@@ -8,6 +8,7 @@ __bash_prompt() {
   local dir_col='\[\e[0;34m\]'
   local git_col='\[\e[1;97m\]'
   local nix_col='\[\e[0;37m\]'
+  local diff_col='\[\e[0;37m\]'
   local punc_col='\[\e[0;97m\]'
   local punc_col2='\[\e[0;37m\]'
   local err_col='\[\e[0;95m\]'
@@ -30,7 +31,11 @@ __bash_prompt() {
   local cwd="${dir_col}${formatted_dir}${reset}"
 
   local git=""
+  local git_add=""
+  local git_mod=""
+  local git_rm=""
   local branch="$(git branch --show-current 2>/dev/null)"
+
   if [ -n "$branch" ]; then
     branch="#${branch}"
     local prefix="$separator_c"
@@ -40,6 +45,20 @@ __bash_prompt() {
       first_separator=0
     fi
     git="${prefix}${git_col}${branch}${reset}"
+
+    while read line; do
+      local file_status=${line:0:1}
+      local added=$(echo $file_status | grep -c 'A')
+      local modified=$(echo $file_status | grep -c 'M')
+      local removed=$(echo $file_status | grep -c 'D')
+      if [ "$added" != 0 ]; then git_add=" +$added"; fi
+      if [ "$modified" != 0 ]; then git_mod=" ~$modified"; fi
+      if [ "$removed" != 0 ]; then git_rm=" -$removed"; fi
+    done <<< $(git diff --name-status HEAD 2>/dev/null)
+
+    if [ -n "$git_add" ] || [ -n "$git_mod" ] || [ -n "$git_rm" ]; then
+      git="${git}${diff_col}${git_add}${git_mod}${git_rm}"
+    fi
   fi
 
   local nix_shell=""
@@ -63,7 +82,7 @@ __bash_prompt() {
 
   local end=""
   if [ "$first_separator" = 0 ]; then
-  end="${separator_d}"
+    end="${separator_d}"
   fi
 
   PS1="${user}${separator_a}${cwd}${git}${nix_shell}${end}\n${prompt_status}${arrow}"
