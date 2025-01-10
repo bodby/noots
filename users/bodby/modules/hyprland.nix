8070d1ed4e5907c70e4ef1581607953797f68c35
Baraa Homsi <baraa.homsi@proton.me>
2025-01-10 10:28:14 +0000

    feat: Made inactive windows more visible

--- users/bodby/modules/hyprland.nix
+++ users/bodby/modules/hyprland.nix
@@ -34,7 +34,7 @@ in
       decoration = {
         rounding = 12;
         active_opacity = 1.0;
-        inactive_opacity = 0.8;
+        inactive_opacity = 0.85;
 
         shadow.enabled = false;
         blur = {
