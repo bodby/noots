cb356bc4f8748fb7915edd8df3ddaec1939a1b85
Baraa Homsi <baraa.homsi@proton.me>
2025-02-23 15:16:56 +0000

    feat(hyprland): Disable window swallowing

--- users/bodby/modules/hyprland.nix
+++ users/bodby/modules/hyprland.nix
@@ -232,7 +232,7 @@ in
         animate_manual_resizes = true;
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
-        enable_swallow = true;
+        enable_swallow = false;
         swallow_regex = "^(foot)$";
         middle_click_paste = false;
         disable_hyprland_qtutils_check = true;
