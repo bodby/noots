6b5678f7cda1ea49e878d070c6260fb1fc0de379
Baraa Homsi <baraa.homsi@proton.me>
2025-03-07 11:54:53 +0000

    feat(neovide): Smaller font size

--- users/bodby/modules/neovide.nix
+++ users/bodby/modules/neovide.nix
@@ -5,6 +5,7 @@
 let
   theme = import ./theme.nix { inherit pkgs; };
 in {
+  # TODO: Make the Neovide font look the same as Foot, esp. line height.
   home-manager.users.bodby.xdg.configFile."neovide/config.toml".source =
     (pkgs.formats.toml { }).generate "config.toml" {
       idle = true;
@@ -14,7 +15,7 @@ in {
 
       font = {
         normal = [ "${theme.fonts.monospace}" ];
-        size = 14;
+        size = 13.5;
         features."${theme.fonts.monospace}" = [ "+ss01" "+ss02" ];
       };
     };
