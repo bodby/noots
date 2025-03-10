7364c77e3ea3c04139cf0870f27fbc107a3b91b6
Baraa Homsi <baraa.homsi@proton.me>
2025-03-10 18:45:37 +0000

    feat(bin): Remove return

--- pkgs/bin/fix-g6-mic.sh
+++ pkgs/bin/fix-g6-mic.sh
@@ -3,5 +3,3 @@
 CARD=$(aplay -l | grep 'Sound BlasterX G6' | cut -d' ' -f 2 | tr -d ':')
 
 amixer -c "$CARD" -q set "PCM Capture Source" "External Mic"
-
-return 0
--- pkgs/bin/fix-laptop-speakers.sh
+++ pkgs/bin/fix-laptop-speakers.sh
@@ -3,5 +3,3 @@
 CARD=$(aplay -l | grep 'HDA Analog' | cut -d' ' -f 2 | tr -d ':' | head -n 1)
 
 amixer -c "$CARD" set Speaker unmute
-
-return 0
